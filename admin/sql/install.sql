0ff4e2e615187b695277034273496a142e369248
matthew murphy <mattyhead@gmail.com>
2019-02-26 08:36:44 -0500

    Let's standardize those varchars

--- admin/sql/install.sql
+++ admin/sql/install.sql
@@ -22,8 +22,8 @@ CREATE TABLE `#__pv_papers` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `hash` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `display_id` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `p_template_html` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `p_template_css` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `p_template_html` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT ''
+  `p_template_css` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT ''
   `p_template_affidavit` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `p_template_instructions` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `p_template_statement` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -122,8 +122,8 @@ DROP TABLE IF EXISTS `#__pv_paper_displays`;
 CREATE TABLE `#__pv_paper_displays` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `data_id` int(11) NOT NULL DEFAULT '0',
-  `p_template_html` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `p_template_css` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `p_template_html` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT ''
+  `p_template_css` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT ''
   `p_template_affidavit` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `p_template_instructions` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `p_template_statement` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -167,8 +167,8 @@ ALTER TABLE `#__pv_paper_displays`
 -- AUTO_INCREMENT for table `#__pv_offices`
 --
 ALTER TABLE `#__pv_offices`
-  ADD `p_template_html` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  ADD `p_template_css` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  ADD `p_template_affidavit` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  ADD `p_template_instructions` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  ADD `p_template_statement` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '';
+  ADD `p_template_html` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' AFTER `template_html`,
+  ADD `p_template_css` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' AFTER `template_html`,
+  ADD `p_template_affidavit` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' AFTER `_template_affidavit`,
+  ADD `p_template_instructions` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' AFTER `template_instructions`,
+  ADD `p_template_statement` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' AFTER `template_statement`;
