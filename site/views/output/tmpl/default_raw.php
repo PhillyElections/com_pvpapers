198d72ff09c1ffaf3ba4d3e942bd826bb051db52
matthew murphy <mattyhead@gmail.com>
2019-02-28 13:05:59 -0500

    ...

--- site/views/output/tmpl/default_raw.php
+++ site/views/output/tmpl/default_raw.php
@@ -17,8 +17,8 @@ $mpdf->SetTitle('Nomination Paper');
 
 // Write some HTML code:
 //$mpdf->WriteHTML($this->html, 2);
-$x=17;
-$x_offset=182;
+$x=15;
+$x_offset=181.5;
 // only show additional pages based on presence of filenames
 if ( $this->data->p_template_form || $this->data->p_template_affidavit || $this->data->p_template_instructions || $this->data->p_template_statement ) {
 	$mpdf->SetImportUse();
