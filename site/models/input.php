906023a3a28d687d3761aa8b30a7101a61819dff
matthew murphy <mattyhead@gmail.com>
2019-03-06 12:08:21 -0500

    ...

--- site/models/input.php
+++ site/models/input.php
@@ -70,23 +70,18 @@ class PvpapersModelInput extends JModel
     public function store($data)
     {
 
-// kludge - we don't pull templates out of display here
-//        $pdisplay = $this->getTable('pdisplay');
-//        $pdisplay->load($data['display_id']);
-//        $pdisplay->load();
-//        $data['p_template_form'] = $pdisplay->p_template_form;
-//        $data['p_template_html'] = $pdisplay->p_template_html;
-//        $data['p_template_css'] = $pdisplay->p_template_css;
-//        $data['p_template_affidavit'] = $pdisplay->p_template_affidavit;
-//        $data['p_template_instructions'] = $pdisplay->p_template_instructions;
-//        $data['p_template_statement'] = $pdisplay->p_template_statement;
-// kludge part 2 -- we just assign templates for now
-        $data['p_template_form'] = 'blank_nomination_paper.pdf';
-        $data['p_template_html'] = 'default.tpl';
-        $data['p_template_css'] = 'default.css';
-        $data['p_template_affidavit'] = '';
-        $data['p_template_instructions'] = '';
-        $data['p_template_statement'] = '';
+        // kludge - we don't have a display_id, so we're setting one.  Better is to 
+        // fetch an active display.
+        $data['display_id'] = 1;
+
+        $pdisplay = $this->getTable('pdisplay');
+        $pdisplay->load($data['display_id']);
+        $data['p_template_form'] = $pdisplay->p_template_form;
+        $data['p_template_html'] = $pdisplay->p_template_html;
+        $data['p_template_css'] = $pdisplay->p_template_css;
+        $data['p_template_affidavit'] = $pdisplay->p_template_affidavit;
+        $data['p_template_instructions'] = $pdisplay->p_template_instructions;
+        $data['p_template_statement'] = $pdisplay->p_template_statement;
 
         $data['published'] = 1;
 
