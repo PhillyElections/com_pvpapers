d1fb65dfc21145f830e9f826951015987441fd1c
matthew murphy <mattyhead@gmail.com>
2019-03-06 08:13:39 -0500

    ...

--- admin/sql/uninstall.sql
+++ admin/sql/uninstall.sql
@@ -2,18 +2,18 @@ SET FOREIGN_KEY_CHECKS = 0;
  
 SELECT @@FOREIGN_KEY_CHECKS;
 
-DROP TABLE IF EXISTS `jos_pv_paper_displays`;
-DROP TABLE IF EXISTS `jos_pv_paper_data`;
-DROP TABLE IF EXISTS `jos_pv_papers`;
+DROP TABLE IF EXISTS `#__pv_paper_displays`;
+DROP TABLE IF EXISTS `#__pv_paper_data`;
+DROP TABLE IF EXISTS `#__pv_papers`;
 
-DELETE FROM `jos_pv_tables` WHERE `name` LIKE "%pv_papers";
-DELETE FROM `jos_pv_tables` WHERE `name` LIKE "%pv_paper_displays";
-DELETE FROM `jos_pv_tables` WHERE `name` LIKE "%pv_paper_data";
+DELETE FROM `#__pv_tables` WHERE `name` LIKE "%pv_papers";
+DELETE FROM `#__pv_tables` WHERE `name` LIKE "%pv_paper_displays";
+DELETE FROM `#__pv_tables` WHERE `name` LIKE "%pv_paper_data";
 
 --
 -- AUTO_INCREMENT for table `jos_pv_offices`
 --
-ALTER TABLE `jos_pv_offices`
+ALTER TABLE `#__pv_offices`
   DROP COLUMN `p_template_form`,
   DROP COLUMN `p_template_html`,
   DROP COLUMN `p_template_css`,
@@ -22,3 +22,30 @@ ALTER TABLE `jos_pv_offices`
   DROP COLUMN `p_template_statement`;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+--
+-- SET FOREIGN_KEY_CHECKS = 0;
+--  
+-- SELECT @@FOREIGN_KEY_CHECKS;
+-- 
+-- DROP TABLE IF EXISTS `jos_pv_paper_displays`;
+-- DROP TABLE IF EXISTS `jos_pv_paper_data`;
+-- DROP TABLE IF EXISTS `jos_pv_papers`;
+-- 
+-- DELETE FROM `jos_pv_tables` WHERE `name` LIKE "%pv_papers";
+-- DELETE FROM `jos_pv_tables` WHERE `name` LIKE "%pv_paper_displays";
+-- DELETE FROM `jos_pv_tables` WHERE `name` LIKE "%pv_paper_data";
+-- 
+-- --
+-- -- AUTO_INCREMENT for table `jos_pv_offices`
+-- --
+-- ALTER TABLE `jos_pv_offices`
+--   DROP COLUMN `p_template_form`,
+--   DROP COLUMN `p_template_html`,
+--   DROP COLUMN `p_template_css`,
+--   DROP COLUMN `p_template_affidavit`,
+--   DROP COLUMN `p_template_instructions`,
+--   DROP COLUMN `p_template_statement`;
+-- 
+-- SET FOREIGN_KEY_CHECKS = 1;
+--
