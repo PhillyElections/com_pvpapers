d1279e4c05c78152eaebda7c683216bbdd0e6c26
matthew murphy <mattyhead@gmail.com>
2019-03-08 11:12:07 -0500

    ...

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -162,7 +162,7 @@ jQuery.noConflict()
             break;
             // this adds district
             case "District City Council":
-                console.log(this.id.substring(2,2))
+                console.log(this.id.substring(this.id.length-3,this.id.length-1))
             break;
             // these add a crapload of stuff...
             case "Committeeperson":
