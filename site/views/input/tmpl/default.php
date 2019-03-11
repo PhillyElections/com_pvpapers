3a1781a9780208cf8d2ab385cd06ce72c0f3ea49
matthew murphy <mattyhead@gmail.com>
2019-03-11 09:37:11 -0400

    ...

--- site/views/input/tmpl/default.php
+++ site/views/input/tmpl/default.php
@@ -173,8 +173,8 @@ if (! count($pdisplays) ) {
             <div>
                 <table class="candidate-header">
                     <tr>
-                        <td class="twenty center">Office</td>
-                        <td class="fourteen center">District</td>
+                        <td class="twenty-two center">Office</td>
+                        <td class="sixteen center">District</td>
                         <td class="seventeen center">Name</td>
                         <td class="seventeen center">Address</td>
                         <td class="fifteen center">Occupation</td>
